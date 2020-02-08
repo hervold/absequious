53cb2d5501eb1748272c322317132671aa32e5f6
kieran hervold <hervold@gmail.com>
2020-02-08 10:35:46 -0800

    basic helper functions

---
+++ absequious/utils/__init__.py
@@ -0,0 +1,44 @@
+from Bio.Seq import Seq
+from enum import Enum
+
+
+class Dir:
+    fwd = 0
+    rev_comp = 1
+
+
+def translate(dna):
+    """
+    translate DNA sequence to protein
+    raises Bio.Data.CodonTable.TranslationError
+    """
+    return str(Seq(dna).translate())
+
+
+def revcomp(dna):
+    """reverse compliment"""
+    return "".join(
+        {"A": "T", "T": "A", "G": "C", "C": "G", "N": "N"}[b] for b in dna.upper()[::-1]
+    )
+
+
+def translate_six(dna):
+    """
+    returns six translations, as tulples:
+        (direction, offset, translation)
+    """
+    ret = []
+    for rc in (False, True):
+        for offset in range(3):
+            # to avoid biopython warnings, we need to trim any partial codons, but we don't
+            # want to @trim to be set to 0, or we'll get an empty string
+            trim = -1 * ((len(dna) - offset) % 3) or None
+
+            ret.append(
+                (
+                    Dir.rev_comp if rc else Dir.fwd,
+                    offset,
+                    translate(revcomp(dna)[offset:trim] if rc else dna[offset:trim]),
+                )
+            )
+    return ret
---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.append(".")
+from absequious import utils
+
+
+def test_translate_six():
+    assert utils.translate_six("ATGCAACCGATGACAAA") == [
+        (utils.Dir.fwd, 0, "MQPMT"),
+        (utils.Dir.fwd, 1, "CNR*Q"),
+        (utils.Dir.fwd, 2, "ATDDK"),
+        (utils.Dir.rev_comp, 0, "FVIGC"),
+        (utils.Dir.rev_comp, 1, "LSSVA"),
+        (utils.Dir.rev_comp, 2, "CHRLH"),
+    ]
